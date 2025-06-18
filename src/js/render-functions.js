0d6bfe6b7c1f4617548dbd40b65cdb21479dfb62
Maksym Semenov <max_semenov@ukr.net>
2025-06-19 00:42:47 +0300

    refactoring

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,6 +3,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
+const loadMoreBtn = document.querySelector(".load-more-button");
 const lightbox = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250, });
 
 export const createGallery = images => {
@@ -34,4 +35,12 @@ export const showLoader = () => {
 
 export const hideLoader = () => {
     loader.classList.add("hidden");
+};
+
+export const showLoadMoreButton = () => {
+    loadMoreBtn.classList.remove("hidden");
+};
+
+export const hideLoadMoreButton = () => {
+    loadMoreBtn.classList.add("hidden");
 };
